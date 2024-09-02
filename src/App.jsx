84a7be68abbd99f1b29b2936e7dba8f66b825b0f
Saddam-ansari-11 <saddam6389046@gmail.com>
2024-09-02 12:21:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./components/Weather";
+import "./App.css"
+const App = () => {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
